051a189e872257ed4f49ffa111436e545d56ed1e
ceronvalentina <chaguendovalentina30@gmail.com>
2024-05-02 12:19:00 -0500

    modified:   css/bulma.min.css 	new file:   css/cabecera.css 	new file:   css/login.css 	new file:   img/D.jpg 	new file:   img/c.jpg 	deleted:    img/proyecto-final.docx 	new file:   php/procesar_fondo.php 	modified:   vistas/home.php 	modified:   vistas/login.php 	modified:   vistas/user_update.php

--- vistas/home.php
+++ vistas/home.php
@@ -1,6 +1,6 @@
 <style>
     .containeeer {
-        background-image: url('img/fondoo.jpg');
+        background-image: url('img/c.jpg');
         background-size: cover;
         background-position: center;
         height: 100vh; /* Esto ajustará la altura de la pantalla al 100% del tamaño de la ventana del navegador */
@@ -13,6 +13,8 @@
 </style>
 
 <div class="containeeer">
-	<h1 class="title">Home</h1>
-	<h2 class="subtitle">¡Bienvenido <?php echo $_SESSION['nombre']." ".$_SESSION['apellido']; ?>!</h2>
+    <h1 class="title">¡Bienvenido <?php echo $_SESSION['nombre']." ".$_SESSION['apellido']; ?>!</h1>
+    <p>"La Simona es más que un simple bar, es un oasis urbano donde puedes escapar del ajetreo de la vida diaria <br>
+        y sumergirte en un ambiente cálido y acogedor. Somos un refugio para aquellos que buscan disfrutar de <br>
+        buena compañía, música agradable y, por supuesto, una gran variedad de bebidas."</p>
 </div>
